d83f7fa65375ede22beca013e073a17204373dec
Hima Varsha Palle <114915084+HimaVarshaPalle@users.noreply.github.com>
2025-01-02 05:54:24 +0530

    Update Basic Calculator.c

--- Basic Calculator.c
+++ Basic Calculator.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
+
 int main() {
     int operation;
     float num1, num2, result;
+
     printf("Select an Operation:\n");
     printf("1. Addition\n");
     printf("2. Subtraction\n");
@@ -11,31 +13,44 @@ int main() {
     printf("5. Percentage\n");
     printf("Enter the number corresponding to the operation: ");
     scanf("%d", &operation);
+
     if (operation >= 1 && operation <= 5) {
         printf("Enter two numbers: ");
-        scanf("%f %f", &num1, &num2);}
-        if (operation == 1) {
-            result = num1 + num2;
-            printf("Result: %.2f\n", result);
-        } else if (operation == 2) {
-            result = num1 - num2;
-            printf("Result: %.2f\n", result);
-        } else if (operation == 3) {
-            result = num1 * num2;
-            printf("Result: %.2f\n", result);
-        } else if (operation == 4) {
-            if (num2 != 0) {
-                result = num1 / num2;
+        scanf("%f %f", &num1, &num2);
+
+        switch (operation) {
+            case 1:
+                result = num1 + num2;
                 printf("Result: %.2f\n", result);
-            } else {
-                printf("Error: Division by zero is not allowed.\n");
-            }
-        } else if (operation == 5) {
-            result = (num1 / num2) * 100;
-            printf("Percentage: %.2f%%\n", result);
+                break;
+            case 2:
+                result = num1 - num2;
+                printf("Result: %.2f\n", result);
+                break;
+            case 3:
+                result = num1 * num2;
+                printf("Result: %.2f\n", result);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    printf("Result: %.2f\n", result);
+                } else {
+                    printf("Error: Division by zero is not allowed.\n");
+                }
+                break;
+            case 5:
+                if (num2 != 0) {
+                    result = (num1 / num2) * 100;
+                    printf("Percentage: %.2f%%\n", result);
+                } else {
+                    printf("Error: Division by zero is not allowed.\n");
+                }
+                break;
         }
     } else {
         printf("Invalid operation selected. Please enter a number between 1 and 5.\n");
     }
-      return 0;
+
+    return 0;
 }
